a78168393899dec0ffcf1d9b50d9c601ac7dd670
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-04-27 20:04:47 +0200

    rendering cards contents, searcshing function

--- main.js
+++ main.js
@@ -1,56 +1,113 @@
+// konstansok létrehozása:
 const $CONTAINER = document.querySelector('.js-container'); // kiválasztunk egy elemet a html elemek közül
-const API_URL = "https://dummyjson.com/recipes?limit=100"; // elmentjuk az API url-jét
+const limit = 100;  // a lekérni kívánt elemek száma
+const API_URL = `https://dummyjson.com/recipes?limit=${limit}`; // elmentjuk az API url-jét
+const INPUT = document.querySelector('.js-input');
+const FORM = document.querySelector('.js-form');
 
 
+const RECIPES = await fetchData(API_URL);
+console.log(RECIPES)
+async function fetchData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.recipes;
+};
+
+
+
+
+
+/*
+// kérés küldése az API-nak:
 fetch(API_URL) // kérést küldünk az url-re
     .then(response => response.json()) //amint válaszolt azután kapunk egy választ amit json formátummá alakítjuk.
     .then((data) => render(data.recipes)); //aztán fogjuk a kapott adatot és csinálunk vele valamit. (átpasszoljuk a render függvénynek)
+// ----------------------------------------------------------------
 
 
+*/
+
+
+
+
+// rating csillagok renderelése:
+//1. Az adtok közül megkapja a rating értéket.
+function createStars(recipeRating) {
+    let rating = Math.floor(recipeRating); // Megkapjuk a reating egész értékét.  Ennyi teljes csillagot biztos meg kell jeleníteni.
+    let rest = Math.round((recipeRating - rating) * 10); // Megkapjuk a rating tizedees értékét. 
+    console.log(recipeRating, rating, rest);
+
+    let starsHTML = '';
+
+    //half star =  <i class="fa-solid fa-star-half-stroke"></i>
+
+    for (let i = 0; i < 5; i++) {
+        if (rating > 0) {
+            starsHTML += `<i class="fa-solid fa-star"></i>`
+            rating--
+        } else if (rest > 0) {
+            if (rest < 3) { // ha kissebb mint három a tizeds akkor egy üres csillagit kap
+                starsHTML += ` <i class="fa-regular fa-star"></i>`
+            } else if (rest < 7) { // ha nagyobb mint 3 de kissebb mint akkor egy fél csillaot kap
+                starsHTML += `<i class="fa-solid fa-star-half-stroke"></i>`
+            } else { // különben ha nagyobb int 7 akkor egy egész csillagot kap
+                starsHTML += `<i class="fa-solid fa-star"></i>`
+            }
+
+        } else {
+            starsHTML += ` <i class="fa-regular fa-star"></i>`
+        }
+
+    };
+
+    return starsHTML;
+};
+
+//---------------------------------------------------------------------
+
+function createParagraphList(list) {
+    return list.map((item) => `<p>${item}</p>`).join("");
+};
+
+
+// ----------------------------------------------------------------
+// a recept kártyák létrehozása:
 function createRecipeTemplate(recipe) {
+    const starsHTML = createStars(recipe.rating);
+    const ingredients = createParagraphList(recipe.ingredients);
+    const instructions = createParagraphList(recipe.instructions);
     return `<div class="recipe">
         <div class="recipe-top">
-            <img src="https://cdn.dummyjson.com/recipe-images/1.webp" alt="Pizza">
+            <img src=${recipe.image} alt="${recipe.name}">
         </div>
         <div class="recipe-bottom">
-            <h2>Pizza</h2>
+            <h2>${recipe.name}</h2>
 
             <div class="recipe-rating flex-around-center ">
                 <div>
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star-half-stroke"></i>
-                    <i class="fa-regular fa-star"></i>
+                ${starsHTML}
                 </div>
-                <p>4.9</p>
+                <p>${recipe.rating}</p>
             </div>
 
 
             <div class="recipe-data flex-around-center ">
                 <i class="fa-regular fa-clock"></i>
-                <p>Prep: <span>15 min</span></p>
-                <p>Cooking: <span>20 min</span></p>
+                <p>Prep: <span>${recipe.prepTimeMinutes} min</span></p>
+                <p>Cooking: <span>${recipe.cookTimeMinutes} min</span></p>
             </div>
 
 
             <div class="recipe-text">
                 <h3>Ingredients:</h3>
                 <div class="recipe-grid">
-                    <p>Pizza dough</p>
-                    <p>Pizza dough</p>
-                    <p>Pizza dough</p>
-                    <p>Pizza dough</p>
-                    <p>Pizza dough</p>
-                    <p>Pizza dough</p>
-                    <p>Pizza dough</p>
+                   ${ingredients}
                 </div>
             </div>
             <div class="recipe-text">
                 <h3>Instruction:</h3>
-                <p>Preheat the oven to 475°F (245°C).</p>
-                <p>Roll out the pizza dough and spread tomato sauce evenly.</p>
-                <p>Top with slices of fresh mozzarella and fresh basil leaves.</p>
+                ${instructions}
             </div>
         </div>
     </div>`
@@ -58,14 +115,37 @@ function createRecipeTemplate(recipe) {
 
 
 
-function render(data) {
-    const recipes = data.map((recipe) => createRecipeTemplate(recipe)).join("");
-    $CONTAINER.innerHTML = recipes;
-};
 
-// Ezt csinálja a render függvény 
-// 1. Megkapjuk az adatokat az api-tól. (data)
+
+
+// ----------------------------------------------------------------
+// Ezt csinálja a render függvény:
+// 1. Megkapjuk az adatokat az api-tól. (data) - (fetch kérésben használjuk fel ezt a függvény)
 // 2. Map-el végig megyünk a kapott tömb összes elemén(objektumok vannak benne) és mindegyikre meghívjuk a 
 //    createRecipeTamplate függvényt amivel egy tamplete stringet kapunk amiket öszzefűzünk a join metódussal. 
 // 3. Ezután ezt elmentjük egy változóba.
 // 4. Ennek a változónak az értékét kiíratjuk a már megkeresett container innerhtml-jébe.
+
+function render(data) {
+    const recipes = data.map((recipe) => createRecipeTemplate(recipe)).join("");
+    $CONTAINER.innerHTML = recipes;
+    //console.log(recipes);
+};
+
+render(RECIPES);
+
+
+// ----------------------------------------------------------------
+function inputEventHandler(e) {
+    const filterValue = e.target.value.toLowerCase();
+    console.log(filterValue);
+    const filterData = RECIPES.filter( recipe => {
+        return (
+            recipe.name.toLowerCase().includes(filterValue)  || recipe.ingredients.join(' ').toLowerCase().includes(filterValue)
+        );
+    });
+
+    render(filterData);
+};
+
+INPUT.addEventListener('input', inputEventHandler)
